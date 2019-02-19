de76508ed1c84bf4756bf131e358d80b667469e7
Joep Meindertsma <joep@argu.co>
2019-02-19 14:08:12 +0100

    SVG logo

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 import ListSearch from "../components/ListSearch";
-import vngLogo from "../images/vng-logo.png";
+import vngLogo from "../images/VNGRealisatieLogo.svg";
 
 const Home: React.FunctionComponent = () => (
   <div>
     <header>
-      <img src={vngLogo} alt="VNG" />
+      <img
+        src={vngLogo}
+        alt="VNG"
+        style={{
+          width: "50px",
+        }}
+      />
       <span>RaadsTalk</span>
     </header>
     <ListSearch />
