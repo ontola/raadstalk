86087ab82c8877a08324ec3ba00696143079c940
Joep Meindertsma <joep@argu.co>
2019-02-19 15:03:04 +0100

    Current search term in results page

--- src/views/List.tsx
+++ src/views/List.tsx
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
 import { withRouter, RouteComponentProps } from "react-router";
 import { Link } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import SearchBar from "../components/SearchBar";
 
 const defaultProps = {
   match: {
     params: {
-      query: 'test'
-    }
-  }
+      query: "test",
+    },
+  },
 };
 
 /** Show the list of municipalities + hit counts for a single query */
 class List extends Component<RouteComponentProps<any>> {
   public render() {
+    const query = this.props.match.params.query;
     return (
       <React.Fragment>
-        <Link to="/">Home</Link>
-        <p>Resultaten voor {this.props.match.params.query}</p>
+        <BackButton/>
+        <SearchBar initialText={query}/>
+        <p>Resultaten voor {query}</p>
       </React.Fragment>
     );
   }
