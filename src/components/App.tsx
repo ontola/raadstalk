dcb45ef5dfac145e1dfe4f95e4f269ff67d5c858
Joep Meindertsma <joep@argu.co>
2019-02-19 15:02:05 +0100

    Remove unnecessary navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,28 +10,12 @@ import "../styles/App.scss";
 
 const App: React.FunctionComponent = () => (
   <BrowserRouter>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/list">List</Link>
-        </li>
-        <li>
-          <Link to="/map">Map</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-      <div id="main-content" className="boundingBox">
-        <Route exact path="/" component={Home} />
-        <Route path="/list/:query" component={List} />
-        <Route path="/map" component={Map} />
-        <Route path="/about" component={About} />
-        <footer><Link to="/about">Over deze app</Link> | Gemaakt met Open Raadsinformatie</footer>
-      </div>
+    <div id="main-content" className="boundingBox">
+      <Route exact path="/" component={Home} />
+      <Route path="/list/:query" component={List} />
+      <Route path="/map" component={Map} />
+      <Route path="/about" component={About} />
+      <footer><Link to="/about">Over deze app</Link> | Gemaakt met Open Raadsinformatie</footer>
     </div>
   </BrowserRouter>
 );
