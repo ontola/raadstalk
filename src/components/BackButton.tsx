0bc5d7d4136290836b5f71488798dde218c4c58d
Joep Meindertsma <joep@argu.co>
2019-02-19 14:42:59 +0100

    Backbutton

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBackward, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import paths from "../paths";
+import { Link } from "react-router-dom";
+import React from "react";
+
+import '../styles/Button.scss';
+
+const BackButton = () =>
+  <Link
+    className="Button"
+    to={paths.home}
+  >
+    <FontAwesomeIcon icon={faArrowLeft} />
+    <span>Terug</span>
+  </Link>;
+
+export default BackButton;
