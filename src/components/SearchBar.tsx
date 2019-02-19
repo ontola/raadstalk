83c82d39551c74bdc6f09553fddee4afe23c7cdf
Joep Meindertsma <joep@argu.co>
2019-02-19 15:02:30 +0100

    Search bar improvements

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,51 +1,52 @@
-import React, { Component, ChangeEvent, FormEvent } from 'react';
-import Radium from 'radium';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import React, { Component, ChangeEvent, FormEvent } from "react";
+import Radium from "radium";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-import { sharedColors, sharedBorderRadius } from '../sharedStyles';
+import { sharedColors, sharedBorderRadius } from "../sharedStyles";
 
-import paths from '../paths';
+import paths from "../paths";
 import "../styles/SearchBar.scss";
-import { withRouter, RouteComponentProps } from 'react-router';
+import { withRouter, RouteComponentProps } from "react-router";
 
 const inputStyle = {
   borderRadius: sharedBorderRadius,
-  borderStyle: 'solid',
+  borderStyle: "solid",
   borderColor: sharedColors.g1,
-  borderWidth: '1px',
-  '::placeholder': {
+  borderWidth: "1px",
+  "::placeholder": {
     color: sharedColors.g1,
   },
-  ':focus': {
+  ":focus": {
     borderColor: sharedColors.blue,
-    outline: 'none',
+    outline: "none",
   },
-  boxSizing: 'border-box' as 'border-box',
-  paddingTop: '4px',
-  paddingBottom: '4px',
-  paddingLeft: '10px',
-  position: 'relative' as 'relative',
-  width: '100%',
-  height: '26px',
+  boxSizing: "border-box" as "border-box",
+  paddingTop: "4px",
+  paddingBottom: "4px",
+  paddingLeft: "10px",
+  position: "relative" as "relative",
+  width: "100%",
+  height: "26px",
 };
 
 const wrapperStyle = {
-  position: 'relative' as 'relative',
+  position: "relative" as "relative",
+};
+
+interface SearchBarProps extends RouteComponentProps {
+  initialText?: string;
 }
 
-interface searchBarProps extends RouteComponentProps {
-};
+interface SearchBarState {
+  text: string;
+}
 
-interface searchBarState {
-  text: string,
-};
-
-class SearchBar extends Component<searchBarProps, searchBarState> {
-  constructor(props: searchBarProps) {
+class SearchBar extends Component<SearchBarProps, SearchBarState> {
+  constructor(props: SearchBarProps) {
     super(props);
     this.state = {
-      text: '',
+      text: this.props.initialText || "",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -54,13 +55,17 @@ class SearchBar extends Component<searchBarProps, searchBarState> {
 
   handleChange(event: ChangeEvent<HTMLInputElement>) {
     this.setState({
-      text: event.target.value
+      text: event.target.value,
     });
   }
 
   handleOnSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    this.props.history.push(paths.list(this.state.text));
+    const text = this.state.text;
+
+    if (text.length > 0) {
+      this.props.history.push(paths.list(this.state.text));
+    }
   }
 
   render() {
@@ -74,6 +79,7 @@ class SearchBar extends Component<searchBarProps, searchBarState> {
           style={inputStyle}
           type="text"
           onChange={this.handleChange}
+          value={this.state.text}
         />
         <button className="SearchBar__search-icon" >
           <FontAwesomeIcon icon={faSearch} />
