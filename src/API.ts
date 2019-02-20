42ccb7b960bca36d8ea8fd2b8e5d0871a4b7eeb4
Joep Meindertsma <joep@argu.co>
2019-02-20 15:32:50 +0100

    API working!

--- src/API.ts
+++ src/API.ts
@@ -1,32 +1,52 @@
 import { ListItemType } from "./types";
+import { number, string } from "prop-types";
 
-export function getSearchResults(query: string): ListItemType[] | null {
+export function getSearchResults(query: string): Promise<ListItemType[] | null> {
   const data = {
     query: {
       match: {
         description: query,
       },
     },
+    aggs: {
+      municipalities: {
+        terms: {
+          field : "_index",
+        },
+      },
+    },
     from: 0,
     size: 10,
   };
 
-  fetch("/search", {
+  return fetch("/search", {
     method: "POST",
     mode: "cors",
     credentials: "same-origin",
     body: JSON.stringify(data),
   })
-  .then((response) => {
-    response.json().then((json) => {
-      const body = json;
-      console.log(body);
-      return(body);
-    }).catch(reason => new Error(reason));
-  }).catch((reason) => {
-    return(mockMunicipalitiesHits);
+  .then(response => response.json())
+  .then(json => bucketsToListItems(json.aggregations.municipalities.buckets));
+}
+
+interface ElasticBucket {
+  doc_count: number;
+  key: string;
+}
+
+function bucketsToListItems(items: ElasticBucket[]): ListItemType[] {
+  return items.map((item) => {
+    const parts = item.key.split("_");
+    const label = parts
+    .slice(1, parts.length - 1)
+    .map(s => `${s.charAt(0).toLocaleUpperCase()}${s.substring(1)}`)
+    .join(" ");
+
+    return {
+      label,
+      hitCount: item.doc_count,
+    };
   });
-  return(mockMunicipalitiesHits);
 }
 
 const mockMunicipalitiesHits: ListItemType[] = [
