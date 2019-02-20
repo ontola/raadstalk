a5cbfe98eba0cbee9af9c93a08755a49cdca4fb7
Joep Meindertsma <joep@argu.co>
2019-02-20 16:03:52 +0100

    List uses hooks

--- src/views/List.tsx
+++ src/views/List.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMap } from "@fortawesome/free-solid-svg-icons";
 import { withRouter, RouteComponentProps } from "react-router";
@@ -8,33 +8,41 @@ import SearchBar from "../components/SearchBar";
 import paths from "../paths";
 import ListSearch from "../components/ListSearch";
 import { getSearchResults } from "../API";
+import { ListItemType } from "../types";
 
 /** Show the list of municipalities + hit counts for a single query */
-class List extends Component<RouteComponentProps<any>> {
-  public render() {
-    const query = this.props.match.params.query;
+const List: React.SFC<RouteComponentProps<any>> = (props) => {
 
-    const listItems = getSearchResults(query);
-    return (
-      <React.Fragment>
-        <header>
-          <BackButton/>
-          <Link
-            to={paths.map(query)}
-            className="Button Button--float-right"
-          >
-            <FontAwesomeIcon icon={faMap} />
-            <span>
-              Kaart
-            </span>
-          </Link>
-        </header>
-        <SearchBar initialText={query}/>
-        <p>Aantal keer besproken in gemeenten:</p>
-        <ListSearch items={listItems}/>
-      </React.Fragment>
-    );
-  }
-}
+  const [items, setItems] = useState<ListItemType[] | null>(null);
+
+  const query = props.match.params.query;
+
+  useEffect(
+    () => {
+      getSearchResults(query).then(result => setItems(result));
+    },
+    [query],
+  );
+
+  return (
+    <React.Fragment>
+      <header>
+        <BackButton/>
+        <Link
+          to={paths.map(query)}
+          className="Button Button--float-right"
+        >
+          <FontAwesomeIcon icon={faMap} />
+          <span>
+            Kaart
+          </span>
+        </Link>
+      </header>
+      <SearchBar initialText={query}/>
+      <p>Aantal keer besproken in gemeenten:</p>
+      <ListSearch items={items}/>
+    </React.Fragment>
+  );
+};
 
 export default withRouter(List);
