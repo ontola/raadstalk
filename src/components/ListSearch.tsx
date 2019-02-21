d4805abdc350e3ebc52b1e5857485b7d93e17885
Joep Meindertsma <joep@argu.co>
2019-02-21 11:49:39 +0100

    Bars represent true relations of hit counts

--- src/components/ListSearch.tsx
+++ src/components/ListSearch.tsx
@@ -12,16 +12,12 @@ interface ListSearchProps {
 export default class ListSearch extends Component<ListSearchProps> {
   public renderListItems() {
     if (this.props.items !== null) {
-      const minWidth = 10.0;
-      const maxWidth = 80.0;
 
       const values = this.props.items.map(({ hitCount }) => hitCount);
-      const minHit = Math.min(...values);
       const maxHit = Math.max(...values);
-      const alpha = (maxWidth - minWidth) / (maxHit - minHit);
 
       const result = this.props.items.map(({label, hitCount}) => {
-        const percentage = alpha * (hitCount - minHit) + minWidth;
+        const percentage = 90 * (hitCount / maxHit);
 
         return (
           <ListItem
