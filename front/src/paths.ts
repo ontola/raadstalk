2215b60048002ff61284a3c60975ae2adc69451e
Joep Meindertsma <joepmeindertsma@gmail.com>
2019-05-28 11:55:14 +0200

    update search path ori

--- front/src/paths.ts
+++ front/src/paths.ts
@@ -5,7 +5,7 @@ const paths = {
   /** Search for a string and see the municipalities with counts in a map*/
   map: (query: string) => `/widget/map/${query}`,
   /** Open OpenRaadsInformatie and search for a specific term */
-  ORISearch: (query: string = "") => `http://ori.argu.co/?searchbox=%22${query}%22`,
+  ORISearch: (query: string = "") => `http://ori.argu.co/?zoekterm=%22${query}%22`,
   contactMail: "mailto:info@vngrealisatie.nl?SUBJECT=RaadsTalk",
   // TODO: make repo public.
   github: "https://github.com",
